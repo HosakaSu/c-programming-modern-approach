f3b47f2e38a156ddafe872f1e1ae17bd74496f42
HosakaSu <liucjseu@outlook.com>
2021-09-10 23:50:25 +0800

    complete section6 project 6

--- 06/project/3.c
+++ 06/project/3.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 
+int gcd(int number1, int number2);
+
 int main(void){
     int number1, number2, number3;
+    printf("Enter a fraction:");
+    scanf("%d/%d", &number1, &number2);
+    number3 = gcd(number1, number2);
+    printf("In lowest term: %d/%d\n", number1/number3, number2/number3);
 
-    printf("Enter two numbers: ");
-    scanf("%d %d", &number1, &number2);
+
+
+
+    return 0;
+}
+
+int gcd(int number1, int number2){
+    int number3;
     if(number1 < number2){
         number3 = number2;
         number2 = number1;
@@ -12,8 +24,7 @@ int main(void){
     }
     if(number2 == 0){
         number3 = number1;
-        printf("Greatest common divisor: %d", number3);
-        return -1;
+        return number3;
     }
     else {
         while(number2 != 0){
@@ -21,11 +32,6 @@ int main(void){
             number1 = number2;
             number2 = number3;
         }
-        printf("Greatest common divisor: %d\n", number1);
+        return number1;
     }
-
-
-
-
-    return 0;
 }
